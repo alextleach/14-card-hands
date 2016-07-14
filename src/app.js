b7df27236f6431417927489212c157157ffb9db5
Alex T Leach <alextleach@gmail.com>
2016-07-14 14:56:17 -0500

    stuck on some kind of class name issue

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 'use strict';
+import Hand from 'hand';
 
 export default class App {
 
-  constructor(element){
+  constructor(element) {
     this.element = element;
   }
 
-  start(){
+  start() {
+    fetch(`https://card-proxy.herokuapp.com/decks/new`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        for (var i = 0; i < 5; i++) {
+          const newHand = new Hand(data.deck_id);
+          this.element.querySelector('.app-container')
+            .appendChild(newHand.element);
 
+          newHand.getData().then(() => {
+            newHand.render();
+          });
+        }
+      });
   }
 }
---
+++ src/hand.js
@@ -0,0 +1,37 @@
+export default class Hand {
+
+  constructor(deckId) {
+    this.deckId = deckId;
+    this.element = document.createElement('div');
+    this.element.classList.add('row');
+  }
+
+  getData() {
+    return fetch(`https://card-proxy.herokuapp.com/decks/${this.deckId}/draw?count=5`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.data = data;
+        console.log(data);
+      });
+  }
+  render() {
+    const cardItem = this.data.cards.map((card) => {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('row');
+
+      cardElement.innerHTML = `
+  <div class="card">
+        <div class="card__image">
+          <img src="${card.image}" alt="" class="card__image__img">
+        </div>
+        <h3 class="card__value">${card.value}</h3>
+      </div>
+  `;
+
+      return cardElement;
+    });
+    cardItem.forEach((cardItem) => {
+      this.element.appendChild(cardItem)
+    });
+  }
+}
